979475e2a87daf974d95462905a9185d4dcc1d9f
texm <texm@users.noreply.github.com>
2023-04-26 08:16:49 +0800

    add base

--- web/src/routes/Screenshots.svelte
+++ web/src/routes/Screenshots.svelte
@@ -1,8 +1,10 @@
 <script>
+	import {base} from "$app/paths";
+
 	const carouselImages = [
-		{alt: "dashboard screenshot", src: "/images/dashboard-light.webp"},
-		{alt: "app overview", src: "/images/app-overview.webp"},
-		{alt: "service overview", src: "/images/service-overview.webp"},
+		{alt: "dashboard screenshot", src: `${base}/images/dashboard-light.webp`},
+		{alt: "app overview", src: `${base}/images/app-overview.webp`},
+		{alt: "service overview", src: `${base}/images/service-overview.webp`},
 	]
 
 	const nextIdx = i => ((i + 1) % carouselImages.length);
@@ -10,6 +12,6 @@
 </script>
 
 <div class="mockup-window rounded-xl bg-base-200 w-auto h-fit shadow-lg shadow-secondary">
-	<img class="inline dark:hidden" alt="dashboard screenshot" src="/images/dashboard-dark.webp" />
-	<img class="hidden dark:inline" alt="dashboard screenshot" src="/images/dashboard-light.webp" />
+	<img class="inline dark:hidden" alt="dashboard screenshot" src="{base}/images/dashboard-dark.webp" />
+	<img class="hidden dark:inline" alt="dashboard screenshot" src="{base}/images/dashboard-light.webp" />
 </div>
