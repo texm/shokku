9d9b968e60e96e52b4306955dd042e7935dee06c
texm <texm@users.noreply.github.com>
2023-04-27 17:38:33 +0800

    npm upgrade

--- ui/src/components/common/Icon.svelte
+++ ui/src/components/common/Icon.svelte
@@ -1,34 +1,36 @@
 <script>
-  import CirclePlus from "tabler-icons-svelte/icons/CirclePlus.svelte";
-  import CircleMinus from "tabler-icons-svelte/icons/CircleMinus.svelte";
-  import Copy from "tabler-icons-svelte/icons/Copy.svelte";
-  import Trash from "tabler-icons-svelte/icons/Trash.svelte";
-  import Cloud from "tabler-icons-svelte/icons/Cloud.svelte";
-  import RotateClockwise2 from "tabler-icons-svelte/icons/RotateClockwise2.svelte";
-  import Stack from "tabler-icons-svelte/icons/Stack.svelte";
-  import Folder from "tabler-icons-svelte/icons/Folder.svelte";
-  import Link from "tabler-icons-svelte/icons/Link.svelte";
-  import Unlink from "tabler-icons-svelte/icons/Unlink.svelte";
-  import DeviceFloppy from "tabler-icons-svelte/icons/DeviceFloppy.svelte";
-  import Pencil from "tabler-icons-svelte/icons/Pencil.svelte";
-  import Tool from "tabler-icons-svelte/icons/Tool.svelte";
-  import Box from "tabler-icons-svelte/icons/Box.svelte";
-  import ChartCircles from "tabler-icons-svelte/icons/ChartCircles.svelte";
-  import LayersSubtract from "tabler-icons-svelte/icons/LayersSubtract.svelte";
-  import InfoCircle from "tabler-icons-svelte/icons/InfoCircle.svelte";
-  import FileText from "tabler-icons-svelte/icons/FileText.svelte";
-  import AlertCircle from "tabler-icons-svelte/icons/AlertCircle.svelte";
-  import BrandGit from "tabler-icons-svelte/icons/BrandGit.svelte";
-  import Database from "tabler-icons-svelte/icons/Database.svelte";
-  import Settings from "tabler-icons-svelte/icons/Settings.svelte";
-  import PlayerPlay from "tabler-icons-svelte/icons/PlayerPlay.svelte";
-  import PlayerStop from "tabler-icons-svelte/icons/PlayerStop.svelte";
-  import Upload from "tabler-icons-svelte/icons/Upload.svelte";
-  import Terminal2 from "tabler-icons-svelte/icons/Terminal2.svelte";
-  import ArrowLeft from "tabler-icons-svelte/icons/ArrowLeft.svelte";
-  import ArrowRight from "tabler-icons-svelte/icons/ArrowRight.svelte";
-  import Key from "tabler-icons-svelte/icons/Key.svelte";
-  import ExternalLink from "tabler-icons-svelte/icons/ExternalLink.svelte";
+  import {
+    IconCirclePlus,
+    IconCircleMinus,
+    IconCopy,
+    IconTrash,
+    IconCloud,
+    IconRotateClockwise2,
+    IconStack,
+    IconFolder,
+    IconLink,
+    IconUnlink,
+    IconDeviceFloppy,
+    IconPencil,
+    IconTool,
+    IconBox,
+    IconChartCircles,
+    IconLayersSubtract,
+    IconInfoCircle,
+    IconFileText,
+    IconAlertCircle,
+    IconBrandGit,
+    IconDatabase,
+    IconSettings,
+    IconPlayerPlay,
+    IconPlayerStop,
+    IconUpload,
+    IconTerminal2,
+    IconArrowLeft,
+    IconArrowRight,
+    IconKey,
+    IconExternalLink
+  } from "@tabler/icons-svelte";
 
   // using https://devicon.dev
   import Postgres from "./devicons/Postgres.svelte";
@@ -47,44 +49,44 @@
   export let size = "md";
 
   const icons = {
-    add: CirclePlus,
-    remove: CircleMinus,
-    delete: Trash,
-    "external-link": ExternalLink,
+    add: IconCirclePlus,
+    remove: IconCircleMinus,
+    delete: IconTrash,
+    "external-link": IconExternalLink,
     docker: Docker,
     go: Go,
     javascript: Javascript,
     python: Python,
     ruby: Ruby,
-    cloud: Cloud,
-    copy: Copy,
-    restart: RotateClockwise2,
-    build: Stack,
-    rebuild: Stack,
-    folder: Folder,
-    link: Link,
-    unlink: Unlink,
-    save: DeviceFloppy,
-    plus: CirclePlus,
-    edit: Pencil,
-    cube: Box,
-    left: ArrowLeft,
-    right: ArrowRight,
-    key: Key,
-    circles: ChartCircles,
-    layers: LayersSubtract,
-    info: InfoCircle,
-    "file-text": FileText,
-    warning: AlertCircle,
-    error: AlertCircle,
-    git: BrandGit,
-    terminal: Terminal2,
-    database: Database,
-    spanner: Tool,
-    settings: Settings,
-    start: PlayerPlay,
-    stop: PlayerStop,
-    upload: Upload,
+    cloud: IconCloud,
+    copy: IconCopy,
+    restart: IconRotateClockwise2,
+    build: IconStack,
+    rebuild: IconStack,
+    folder: IconFolder,
+    link: IconLink,
+    unlink: IconUnlink,
+    save: IconDeviceFloppy,
+    plus: IconCirclePlus,
+    edit: IconPencil,
+    cube: IconBox,
+    left: IconArrowLeft,
+    right: IconArrowRight,
+    key: IconKey,
+    circles: IconChartCircles,
+    layers: IconLayersSubtract,
+    info: IconInfoCircle,
+    "file-text": IconFileText,
+    warning: IconAlertCircle,
+    error: IconAlertCircle,
+    git: IconBrandGit,
+    terminal: IconTerminal2,
+    database: IconDatabase,
+    spanner: IconTool,
+    settings: IconSettings,
+    start: IconPlayerPlay,
+    stop: IconPlayerStop,
+    upload: IconUpload,
     postgres: Postgres,
     redis: Redis,
     sqlite: SQLite,
