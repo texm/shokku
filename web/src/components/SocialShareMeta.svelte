ebc969a351ceeeb89d75853e720bb47e4e4e5e84
texm <texm@users.noreply.github.com>
2023-05-13 13:07:44 +0800

    fix social

--- web/src/components/SocialShareMeta.svelte
+++ web/src/components/SocialShareMeta.svelte
@@ -2,12 +2,10 @@
 	export let title = "";
 </script>
 
-<svelte:head>
-	<meta property="og:title" content={title}>
-	<meta property="og:type" content="website" />
-	<meta property="og:image" content="https://shokku.dev/share-card.png">
-	<meta name="twitter:card" content="https://shokku.dev/share-card.png">
+<meta property="og:title" content={title}>
+<meta property="og:type" content="website" />
+<meta property="og:image" content="https://shokku.dev/share-card.png">
+<meta name="twitter:card" content="https://shokku.dev/share-card.png">
 
-	<meta property="og:description" content="Free and open source interface for the Dokku PaaS">
-	<meta name="twitter:image:alt" content="Shokku Logo">
-</svelte:head>
+<meta property="og:description" content="Free and open source interface for the Dokku PaaS">
+<meta name="twitter:image:alt" content="Shokku Logo">
